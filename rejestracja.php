ab7ad3b95742f48b14f4e61e9038b0a1345e741d
Rustyiller <36889034+Rustyiller@users.noreply.github.com>
2018-06-04 13:43:11 +0200

    zzzzzzzzz

---
+++ rejestracja.php
@@ -0,0 +1,61 @@
+﻿<?php
+session_start();
+//require_once "connect.php";
+
+	$host = "mysql.cba.pl";
+$log = "julepp";
+$haslo = "Jeffkaplan00";
+$db_name = "julepp";
+	/*$user = $_POST["Username"];
+	$name = $_POST["Name"];
+	$surname = $_POST["Surname"];
+	$email = $_POST["Email"];
+	$pass = $_POST["Password"];
+	$confirm = $_POST["ConfirmPassword"];
+	
+if(isset($_POST["Email"]))
+{
+	$wszystko_ok=true;
+
+	if((strlen($user)<3) || (strlen($user)>20))
+		$wszystko_ok=false;
+	if((strlen($pass)<8) || (strlen($pass)>20))
+		$wszystko_ok=false;
+	if($pass!=$confirm)
+		$wszystko_ok=false;
+	if($wszystko_ok==true){
+		echo "Udana rejestracja";
+	}
+	else{
+		echo "Rejestracja nie udana";
+		header('Location:../index.html');
+	}
+}
+else {
+	echo "Rejestracja nie udana";
+	header('Location:../index.html');
+}
+*/
+
+
+$conn = new mysqli($host, $log, $haslo, $db_name);
+var_dump($conn);
+/*if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+else{
+
+	$sql = "INSERT INTO uzytkownik 
+			VALUES (NULL, '$user', '$name', '$surname', '$email', '$pass')";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+		//header('Location:../index.html');
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+		
+$conn->close();
+}
+*/
+?>
